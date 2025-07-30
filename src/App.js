fd71ce5e1aaa1cb30fc57d7b5513c849e14632c8
Anshul <anshul.rawat@agicent.in>
2025-07-30 17:42:05 +0530

    Add backend source code

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const addUser = async () => {
     if (!name) return alert("Name required");
 
-    const res = await fetch(`${API_URL}/api/users`, {
+    const res = await fetch(`${API_URL}api/users`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name }),
@@ -29,7 +29,7 @@ function App() {
   // }, []);
   useEffect(() => {
     const fetchUsers = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/users`);
       const data = await res.json();
       setUsers(data);
     };
